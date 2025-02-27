6b8536e2992fd0c0edd5c94f431cd75ce041b7ed
huangqiuhuang <qhuang05@sina.com>
2025-02-27 14:49:50 +0800

    init

---
+++ vite.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite'
+import { resolve } from 'path'
+
+export default defineConfig({
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      name: 'QToolsLib',
+      fileName: (format) => `q-tools-lib.${format}.js`
+    },
+    rollupOptions: {
+      external: [], // 添加需要外部化的依赖
+      output: {
+        globals: {} // 为外部化的依赖提供全局变量
+      }
+    }
+  }
+})
